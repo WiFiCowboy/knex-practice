6ee94a201a6e08da88d839f31564378dd3e61416
Humberto Hernandez <hamburger45@gmail.com>
2020-02-10 20:20:55 -0800

    added paginate function for DB

--- src/practice.js
+++ src/practice.js
@@ -32,4 +32,19 @@ function searchByProduceName(searchTerm) {
 		});
 }
 
-searchByProduceName('holo');
+// searchByProduceName('holo');
+
+function paginateProducts(page) {
+	const productsPerPage = 10;
+	const offset = productsPerPage * (page - 1);
+	knexInstance
+		.select('product_id', 'name', 'price', 'category')
+		.from('amazong_products')
+		.limit(productsPerPage)
+		.offset(offset)
+		.then((result) => {
+			console.log(result);
+		});
+}
+
+paginateProducts(2);
